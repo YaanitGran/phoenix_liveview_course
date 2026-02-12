440fc5b0f4b1007102d94a2f55b01b52fe585767
andrew <andrewalangm@gmail.com>
2026-02-12 12:50:31 -0600

    Play battle sound

--- assets/js/pokemon.hooks.js
+++ assets/js/pokemon.hooks.js
@@ -1,8 +1,14 @@
+const BATTLE_SOUND = "/sounds/pokemon_battle.mp3";
 const PokemonBattle = {
   mounted() {
     this.handleEvent("battle:start", (payload) => {
-      console.log(payload);
+      this.playSound(BATTLE_SOUND, 5);
     });
+  },
+  playSound(src, duration) {
+    const audio = new Audio(src);
+    audio.duration = duration;
+    audio.play();
   },
 };
 
